92ec5260f1b299c0e7033756c3f61c9891529545
Manuel Kreutz <manuel@140.am>
2012-05-17 04:33:25 -0400

    refactor broker client to live in its own class space

--- pp_client.py
+++ pp_client.py
@@ -1,6 +1,8 @@
-""" The Client (Task Ventilator) will get pending "Jobs" requests from DB backend and send to the Job Broker
+""" Encoding Booth Broker Client
 
-A socket of type ZMQ_REQ is used by a client to send requests to and receive replies from a service. Each request sent is round-robined among all services, and each reply received is matched with the last issued request.
+A zeromq socket of type ZMQ_REQ is used by a client to send requests to and receive replies from a service.
+Each request sent is round-robined among all services, and each reply received is matched with the
+last issued request.
 """
 
 import time
@@ -10,123 +12,144 @@ import random
 import cjson
 import zmq
 
-BROKER_ENDPOINT = 'tcp://127.0.0.1:5555'
-
-REQUEST_TIMEOUT = 2500
-REQUEST_RETRIES = 5
-REQUEST_LIFESPAN = 5  # seconds
-
 logging.basicConfig( format = '%(asctime)s - %(levelname)s - %(threadName)s - %(message)s' )
 log = logging.getLogger('client')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
-context = zmq.Context(1)
-
-poll = zmq.Poller()
-
-sequence = 0
-retries_left = REQUEST_RETRIES
+# the maximum total `send` blocking time is REQUEST_RETRIES*REQUEST_TIMEOUT in msec
+REQUEST_RETRIES = 3
+REQUEST_TIMEOUT = 1500
 
 
-def setup_router_socket(context, poller):
+class EBClient(object):
 
-    identity = "client-%04X-%04X" % (random.randint(0, 0x10000), random.randint(0, 0x10000))
+    identity = None
+    broker = None  # example: tcp://127.0.0.1:5555
+    context = None
+    client = None
+    poller = None
+    retries = REQUEST_RETRIES  # count - number of request retries
+    timeout = REQUEST_TIMEOUT  # msec - request timeout
+    sequence = 0
 
-    # create a ZMQ_REQ socket to send requests / receive replies
-    client = context.socket(zmq.REQ)
+    def __init__(self, broker):
 
-    # set the `identity` UUID as the worker identify
-    client.setsockopt(zmq.IDENTITY, identity)
-    client.setsockopt(zmq.HWM, 0)
+        self.identity = "client-%04X-%04X" % ( random.randint(0, 0x10000), random.randint(0, 0x10000) )
+        self.broker = broker
+        self.context = zmq.Context(1)
+        self.poller = zmq.Poller()
+        self.connect_to_broker()
 
-    poll.register(client, zmq.POLLIN)
+    def connect_to_broker(self):
 
-    # connect to `Router` socket
-    client.connect(BROKER_ENDPOINT)
+        # close existing socket
+        if self.client:
+            self.poller.unregister(self.client)
+            self.client.setsockopt(zmq.LINGER, 0)
+            self.client.close()
 
-    return client
+        # create a ZMQ_REQ socket to send requests / receive replies
+        self.client = self.context.socket(zmq.REQ)
 
+        # set the `identity` to retrieve responses on disconnect
+        self.client.setsockopt(zmq.IDENTITY, self.identity)
+        self.client.setsockopt(zmq.HWM, 0)
 
-client = setup_router_socket(context, poll)
+        # register socket with poller
+        self.poller.register(self.client, zmq.POLLIN)
 
+        # connect to `Router` socket
+        self.client.connect(self.broker)
 
-while retries_left:
-    sequence += 1
+    def send(self, service, request):
+        """
+        Returns the reply message or None if there was no reply within timeout limit
+        """
+        time_s = time.time()
 
-    """ Request Layout:
+        reply = None
 
-    Frame 0: Empty (zero bytes, invisible to REQ application)
-    Frame 1: "MDPC01" (six bytes, representing MDP/Client v0.1 or Worker etc)
-    Frame 2: Service name (printable string)
-    Frame 3: Expiration Time (unix time in future)
-    Frames 4+: Request body (opaque binary)
-    """
+        self.sequence += 1
 
-    request = [
-        'MDPC01:%s' % str(sequence),
-        'video.cut',
-        str(int(time.time()) + REQUEST_LIFESPAN),
-        cjson.encode({
-            'source_uri' : 'http://www.path.to/mp4/test_%i.mp4' % random.randint(10000, 90000),
-            'profile_name' : 0,
-            'thumb_count' : 0,
-            'thumb_type' : '',
-            'thumb_format' : '%s.jpg',
-            'thumb_size' : '0x0',
-            'output_destination' : '',
-            'notes' : ''
-        })
-    ]
+        while self.retries:
 
-    log.info("Attempting Request #%i (%s)" % (sequence, request))
+            # request timeout
+            #timeout = int(time.time()) + int(REQUEST_TIMEOUT / 1000)
+            timeout = int(time.time()) + 1
 
-    client.send_multipart(request)
+            # convert request body dict to json structure
+            if isinstance(request, dict):
+                request = cjson.encode(request)
 
-    # attempt to send request until reply received or REQUEST_RETRIES timeout
-    expect_reply = True
-    while expect_reply:
+            """ Request Layout:
 
-        socks = dict(poll.poll(REQUEST_TIMEOUT))
+            Frame 0: Empty (zero bytes, invisible to REQ application)
+            Frame 1: "EB1:%i" (3+ bytes, representing version and request sequence number)
+            Frame 2: Service name (printable string)
+            Frame 3: Expiration Time (unix time in future)
+            Frames 4+: Request body (opaque binary, will be converted to json string)
+            """
 
-        if socks.get(client) == zmq.POLLIN:
+            msg = [
+                'EBv1:%i' % self.sequence,
+                str(service),
+                str(timeout),
+                str(request)
+            ]
 
-            reply = client.recv()
-            response = client.recv()
-            timeout = client.recv()
-            config = client.recv()
+            resp = self.client.send_multipart(msg)
 
-            if not reply:
-                log.warn('got empty reply back from `Broker`')
+            try:
+                socks = dict(self.poller.poll(self.timeout))
+            except KeyboardInterrupt:
+                self.shutdown()
                 break
 
-            # compares request sequenze id to be in order
-            if int(reply.split(':')[1]) == sequence:
-                log.info("Server replied OK: %s / %s / %s" % (reply, response, config))
+            if socks.get(self.client) == zmq.POLLIN:
 
-                # TODO: update db record here with status, worker server etc
+                reply = self.client.recv()
+                response = self.client.recv()
+                timeout = self.client.recv()
+                config = self.client.recv()
 
-                retries_left = REQUEST_RETRIES
-                expect_reply = False  # continue with next request
+                # Don't try to handle errors, just assert noisily
+                # assert len(msg) >= 3
+                if not reply:
+                    log.warn('got empty reply back from `Broker`')
+                    break
+
+                # compares request sequence id to be in order
+                if int(reply.split(':')[1]) == self.sequence:
+                    reply = config
+                    self.retries = REQUEST_RETRIES
+                    break
+                else:
+                    log.error("Malformed reply from server: %s / %s" % (self.sequence, reply.split(':')[1]))
+                    reply = None
+                    break
+
             else:
-                log.error("Malformed reply from server: %s / %s" % (sequence / reply))
-                break
-        else:
 
-            # wait up to REQUEST_RETRIES*buffer-timeout for acceptance of the request
-            retries_left -= 1
-            if retries_left == 0:
-                log.warn('re-connecting')
+                self.retries -= 1
 
-                # Socket is confused. Close and remove it.
-                client.setsockopt(zmq.LINGER, 0)
-                client.close()
-                poll.unregister(client)
+                self.connect_to_broker()
 
-                # Create new connection
-                client = setup_router_socket(context, poll)
+                # wait up to REQUEST_RETRIES*buffer-timeout for acceptance of the request
+                if self.retries >= 1:
+                    log.debug('no response from router - re-attempting request')
+                else:
+                    log.debug('no response from router - giving up')
+                    self.retries = REQUEST_RETRIES
+                    break
 
-                retries_left = REQUEST_RETRIES
-                break
+        # messure request time and ensure request takes at least REQUEST_TIMEOUT
+        runtime = time.time() - time_s
+        runtime = runtime * 100000  # conver to msec
+        if runtime < REQUEST_TIMEOUT:
+            time.sleep((REQUEST_TIMEOUT - runtime) / 1000)
 
-context.term()
+        return reply
+
+    def shutdown(self):
+        self.context.term()
 
