ffdab1e01af5140d56b05a17e64668092b28b5b5
Manuel Kreutz <manuel@140.am>
2012-06-09 00:41:43 -0400

    add "Task Result Sink" concept to have a unified interface to receive and parse Worker messages/events

--- ebwrkapi/__init__.py
+++ ebwrkapi/__init__.py
@@ -1,5 +1,6 @@
 from client import EBClient
 from broker import EBBroker
 from worker import EBWorker
+from sink import EBSink
 
 __version__ = 'EBv1'
---
+++ ebwrkapi/sink.py
@@ -0,0 +1,62 @@
+""" Task Result Sink
+
+Provides a PULL zeromq socket to receive Worker messages/events
+
+Example Usage:
+
+sink = ebwrkapi.EBSink('tcp://*:5558')
+while True:
+    msg = sink.get_message()
+    log.info('MSG received: %s' % msg)
+
+worker = ebwrkapi.EBWorker("tcp://localhost:5556", 'image.validate')
+worker.sink.send('task received')
+"""
+
+import logging
+import time
+import random
+import threading
+import zmq
+
+log = logging.getLogger(__name__)
+
+
+class EBSink(object):
+
+    context = None
+    poller = None
+    sink = None
+
+    bind_host = None
+
+    def __init__(self, bind_host = '*:5558'):
+
+        self.context = zmq.Context(1)
+        self.poller = zmq.Poller()
+
+        # PULL socket to receive broadcast/flow messages on
+        self.sink = self.context.socket(zmq.PULL)
+
+        # setting an identity so worker will buffer outgoing messages in case of downtime
+        self.sink.setsockopt(zmq.IDENTITY, 'sink')
+
+        self.sink.bind(bind_host)
+
+        self.setup_polling()
+
+    def setup_polling(self):
+
+        self.poller.register(self.sink, zmq.POLLIN)
+
+    def get_message(self):
+
+        msg = ''
+
+        socks = dict(self.poller.poll())
+
+        if self.sink in socks and socks[self.sink] == zmq.POLLIN:
+            msg = self.sink.recv()
+
+        return msg
+
